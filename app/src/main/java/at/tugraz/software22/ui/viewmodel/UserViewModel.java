d4f72545c52d483c9cd8454ea0cd5feb1c334c1c
Dominik König <d.koenig@student.tugraz.at>
2022-05-18 14:27:35 +0200

    WPL-13 GREEN implement UserViewModel.java [DK, RB]

---
+++ app/src/main/java/at/tugraz/software22/ui/viewmodel/UserViewModel.java
@@ -0,0 +1,60 @@
+package at.tugraz.software22.ui.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import at.tugraz.software22.WhereWareApplication;
+import at.tugraz.software22.domain.User;
+import at.tugraz.software22.service.UserService;
+
+public class UserViewModel extends AndroidViewModel {
+
+    private final UserService userService;
+    private final Executor executor;
+
+    private final MutableLiveData<List<User>> userLiveData = new MutableLiveData<>();
+
+    public UserViewModel(Application application){
+        super(application);
+        WhereWareApplication whereWareApplication = (WhereWareApplication) getApplication();
+        userService = whereWareApplication.getUserService();
+        executor = whereWareApplication.getBackgroundExecutor();
+    }
+
+    public LiveData<List<User>> getUserLiveData() {return userLiveData;}
+
+    public void loadData() {
+        executor.execute(() -> {
+            List<User> users = userService.getAll();
+            this.userLiveData.postValue(users);
+        });
+    }
+
+    public void createUser(User user) {
+        executor.execute(() -> {
+            userService.addUser(user);
+            List<User> users = userService.getAll();
+            this.userLiveData.postValue(users);
+        });
+    }
+    public void removeUser(Integer id) {
+        executor.execute(() -> {
+            userService.removeUser(id);
+            List<User> users = userService.getAll();
+            this.userLiveData.postValue(users);
+        });
+    }
+    public void updateUser(Integer id, User user) {
+        executor.execute(() -> {
+            userService.updateUser(id, user);
+            List<User> users = userService.getAll();
+            this.userLiveData.postValue(users);
+        });
+    }
+}
