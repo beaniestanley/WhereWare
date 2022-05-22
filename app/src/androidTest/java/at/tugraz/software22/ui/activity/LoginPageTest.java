9e9558b40b1fe573fb011455322cd6bca5ecdd54
Rafael Forsthofer <rafael.forsthofer@student.tugraz.at>
2022-05-22 20:45:25 +0200

    WPL-27 REFACTOR deleted unused import and empty lines [YB,RF]

--- app/src/androidTest/java/at/tugraz/software22/ui/activity/LoginPageTest.java
+++ app/src/androidTest/java/at/tugraz/software22/ui/activity/LoginPageTest.java
@@ -1,49 +1,30 @@
 package at.tugraz.software22.ui.activity;
 
-import static androidx.test.espresso.Espresso.onView;
-
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.app.Activity;
 import android.content.res.Resources;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import androidx.test.rule.ActivityTestRule;
-
-
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import at.tugraz.software22.R;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-
-
-
 
 
 @RunWith(AndroidJUnit4.class)
 public class LoginPageTest{
-
-
 
 
     private Resources resources;
@@ -65,7 +46,6 @@ public class LoginPageTest{
                 .check(matches(ViewMatchers.isDisplayed()));
     }
 
-
     @Test
     public void EspressoTest_whenRightLoginData_thenManagerActivityIsStarted() throws InterruptedException {
         Intents.init();
@@ -73,20 +53,17 @@ public class LoginPageTest{
         ActivityScenario.launch(MainActivity.class);
         Espresso.onView(ViewMatchers.withId(R.id.login_name)).perform(ViewActions.clearText()).perform(ViewActions.typeText("admin"));
         Espresso.onView(ViewMatchers.withId(R.id.login_passwd)).perform(ViewActions.clearText()).perform(ViewActions.typeText("admin"));
-
         Espresso.onView(ViewMatchers.withText("LOGIN")).perform(click());
 
         intended(hasComponent(ManagerActivity.class.getName()));
         Intents.release();
     }
 
-
     @Test
     public void EspressoTest_whenRightLoginData_thenOrderActivityIsStarted() throws InterruptedException {
         Intents.init();
 
         ActivityScenario.launch(MainActivity.class);
-
         Espresso.onView(ViewMatchers.withId(R.id.login_name)).perform(ViewActions.clearText()).perform(ViewActions.typeText("employee"));
         Espresso.onView(ViewMatchers.withId(R.id.login_passwd)).perform(ViewActions.clearText()).perform(ViewActions.typeText("employee"));
         Espresso.onView(ViewMatchers.withText("LOGIN")).perform(click());
