fec00467ea8222f084da718cbb254938a203cbe6
Rafael Forsthofer <rafael.forsthofer@student.tugraz.at>
2022-05-25 08:57:40 +0200

    WPL-27 MAINTENANCE add not previously pushed files [YR, RF]

--- app/src/main/java/at/tugraz/software22/ui/activity/MainActivity.java
+++ app/src/main/java/at/tugraz/software22/ui/activity/MainActivity.java
@@ -1,22 +1,24 @@
 package at.tugraz.software22.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import at.tugraz.software22.R;
 import at.tugraz.software22.domain.Order;
 import at.tugraz.software22.ui.adapter.OrderAdapter;
 import at.tugraz.software22.ui.viewmodel.OrderViewModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private OrderViewModel orderViewModel;
     private ArrayAdapter<Order> adapter;
@@ -25,23 +27,52 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_page);
 
-        orderViewModel = new ViewModelProvider(this).get(OrderViewModel.class);
-        setupListView();
-        orderViewModel.getOrderLiveData().observe(this, orders -> {
-            TextView createdSprint = findViewById(R.id.amountOfOrders);
-            createdSprint.setText(getString(R.string.unclosed_orders, orders.size()));
-            adapter.clear();
-            adapter.addAll(orders);
-        });
-        orderViewModel.loadData();
+        Button button= (Button) findViewById(R.id.login_button);
+
+        View.OnClickListener btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                login();
+            }
+        };
+        button.setOnClickListener(btnListener);
+
     }
 
-    private void setupListView() {
-        ListView listView = findViewById(R.id.allOrders);
-        adapter = new OrderAdapter(this, R.layout.list_item);
-        listView.setAdapter(adapter);
+    public void login()
+    {
+        System.out.println("Login Started");
+
+        EditText loginName = findViewById(R.id.login_name);
+        EditText loginPasswd = findViewById(R.id.login_passwd);
+
+        System.out.println("Name = " + loginName.getText());
+        System.out.println("Password = " + loginPasswd.getText());
+        System.out.println("Admin = " + getString(R.string.admin));
+        System.out.println("Employee = " + getString(R.string.employee));
+
+        if (loginName.getText().toString().equals(getString(R.string.admin)) && loginPasswd.getText().toString().equals(getString(R.string.admin)))
+        {
+            //Admin login
+            System.out.println("Admin Login");
+            Intent intent = new Intent(MainActivity.this, ManagerActivity.class);
+            startActivity(intent);
+        }
+        else if (loginName.getText().toString().equals(getString(R.string.employee)) && loginPasswd.getText().toString().equals(getString(R.string.employee)))
+        {
+            //Employee login
+            System.out.println("Employee Login");
+            Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            //Invalid login
+            System.out.println("Invalid Login");
+            Toast.makeText(getApplicationContext(), getString(R.string.invalid_login), Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
