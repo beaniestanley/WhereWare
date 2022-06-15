e4ff94622072c6774c195c746c0c92a8a027a080
Martin Rabensteiner <martin.rabensteiner@student.tugraz.at>
2022-06-15 08:49:51 +0200

    WPL-21 RED test the return of an order repository time frame filter [MR, YB]

---
+++ app/src/test/java/at/tugraz/software22/domain/ReportTest.java
@@ -0,0 +1,94 @@
+package at.tugraz.software22.domain;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Or;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import at.tugraz.software22.service.OrderService;
+import at.tugraz.software22.domain.OrderRepository;
+
+public class ReportTest {
+
+    @Mock
+    private OrderRepository orderRepositoryMock;
+
+    private OrderService orderService;
+
+    public void setUp() {
+        orderService = new OrderService(orderRepositoryMock);
+    }
+
+    @Test
+    public void givenTimeFrame_whenGetOrders_onlyDeliverAskedOrders() {
+        List<Product> emptyList = new ArrayList<Product>();
+        List<Order> fullList = new ArrayList<Order>();
+        List<Order> expectedList = new ArrayList<Order>();
+
+        Order o1 = new Order(1, emptyList, 1);
+        Order o2 = new Order(1, emptyList, 2);
+        Order o3 = new Order(1, emptyList, 3);
+
+        o1.setStartTime(LocalDateTime.of(2022, 6, 1, 0, 0, 0));
+        o1.setEndTime(LocalDateTime.of(2022, 6, 2, 0, 0, 0));
+        o2.setStartTime(LocalDateTime.of(2022, 6, 1, 0, 0, 0));
+        o2.setEndTime(LocalDateTime.of(2022, 6, 2, 0, 0, 0));
+        o3.setStartTime(LocalDateTime.of(2022, 7, 1, 0, 0, 0));
+        o3.setEndTime(LocalDateTime.of(2022, 7, 2, 0, 0, 0));
+
+        fullList.add(o1);
+        fullList.add(o2);
+        fullList.add(o3);
+
+        expectedList.add(o1);
+        expectedList.add(o2);
+
+        Mockito.when(orderRepositoryMock.getAll()).thenReturn(fullList);
+
+        LocalDateTime startDate = LocalDateTime.of(2022, 6, 1, 0, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(2022, 6, 30, 0, 0, 0);
+
+        List<Order> actualOrders = orderService.getOrdersFromTimeframe(startDate, endDate);
+
+        Assert.assertEquals(expectedList, actualOrders);
+    }
+
+    @Test
+    public void givenTimeFrameWithoutOrders_whenGetOrders_deliverNoOrders() {
+        List<Product> emptyList = new ArrayList<Product>();
+        List<Order> fullList = new ArrayList<Order>();
+        List<Order> expectedList = new ArrayList<Order>();
+
+        Order o1 = new Order(1, emptyList, 1);
+        Order o2 = new Order(1, emptyList, 2);
+        Order o3 = new Order(1, emptyList, 3);
+
+        o1.setStartTime(LocalDateTime.of(2022, 6, 1, 0, 0, 0));
+        o1.setEndTime(LocalDateTime.of(2022, 6, 2, 0, 0, 0));
+        o2.setStartTime(LocalDateTime.of(2022, 6, 1, 0, 0, 0));
+        o2.setEndTime(LocalDateTime.of(2022, 6, 2, 0, 0, 0));
+        o3.setStartTime(LocalDateTime.of(2022, 7, 1, 0, 0, 0));
+        o3.setEndTime(LocalDateTime.of(2022, 7, 2, 0, 0, 0));
+
+        fullList.add(o1);
+        fullList.add(o2);
+        fullList.add(o3);
+
+        Mockito.when(orderRepositoryMock.getAll()).thenReturn(fullList);
+
+        LocalDateTime startDate = LocalDateTime.of(1990, 6, 1, 0, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(1990, 6, 30, 0, 0, 0);
+
+        List<Order> actualOrders = orderService.getOrdersFromTimeframe(startDate, endDate);
+
+        Assert.assertEquals(expectedList, actualOrders);
+    }
+}
