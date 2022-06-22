8fec47de8aedb7b1f3fb7ee80b8758762e3dc2b7
Hannes Großauer <hannes.grossauer@student.tugraz.at>
2022-06-22 10:03:14 +0200

    WPL-17 REFACTOR Remove unnecessary comments [HG, RB]

--- app/src/main/java/at/tugraz/software22/service/OrderService.java
+++ app/src/main/java/at/tugraz/software22/service/OrderService.java
@@ -39,35 +39,4 @@ public class OrderService {
     public void updateProduct(Integer id, String product_name, Integer estimated_time, String location, Integer quantity)
     {orderRepository.updateProduct(id, product_name, estimated_time, location, quantity); }
 
-    //TODO afterwards could be used for changing status of products
-//    public void changeStateOfOrder(Integer id, String status){
-//        List<Order> allOrders = orderRepository.getAll();
-//        switch (status){
-//            case "pending":
-//                allOrders.get(id-1).setStatus(Statuses.PENDING);
-//                break;
-//            case "started":
-//                allOrders.get(id-1).setStatus(Statuses.STARTED);
-//                break;
-//            case "finished":
-//                allOrders.get(id-1).setStatus(Statuses.FINISHED);
-//                break;
-//        }
-//    }
-//
-//    public void changeStateOfProductInOrder(Integer orderId,Integer productId, String status){
-//        List<Order> allOrders = orderRepository.getAll();
-//        Product product = allOrders.get(orderId-1).getAllOrders_().get(productId-1);
-//        switch (status){
-//            case "pending":
-//                product.setStatus(Statuses.PENDING);
-//                break;
-//            case "started":
-//                product.setStatus(Statuses.STARTED);
-//                break;
-//            case "finished":
-//                product.setStatus(Statuses.FINISHED);
-//                break;
-//        }
-//    }
 }
