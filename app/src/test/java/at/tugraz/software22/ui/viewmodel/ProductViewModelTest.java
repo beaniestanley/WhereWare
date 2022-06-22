dd80aef67f942544b753fffa85a394d648d8a1a2
Hannes Großauer <hannes.grossauer@student.tugraz.at>
2022-06-22 10:21:38 +0200

    WPL-17 RED Implement Tests for ProductViewModel [HG, RB]

--- app/src/test/java/at/tugraz/software22/ui/viewmodel/ProductViewModelTest.java
+++ app/src/test/java/at/tugraz/software22/ui/viewmodel/ProductViewModelTest.java
@@ -12,11 +12,13 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
 
 import at.tugraz.software22.domain.Order;
 import at.tugraz.software22.domain.Product;
+import at.tugraz.software22.domain.User;
 import at.tugraz.software22.service.OrderService;
 import at.tugraz.software22.WhereWareApplication;
 
@@ -61,5 +63,54 @@ public class ProductViewModelTest {
         productViewModel.loadData(1);
 
         Mockito.verify(productsLiveDataObserver).onChanged(expectedProducts);
+    }
+
+    @Test
+    public void givenOrderServiceWithTwoProducts_whenLoadProductData_thenVerifyProductsLiveDataChanged() {
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(new Product(12, 3, "Superproduct", "Storage 5", 1));
+        expectedProducts.add(new Product(5, 2, "Topseller", "Storage 2", 2));
+
+        Mockito.when(orderServiceMock.getProducts()).thenReturn(expectedProducts);
+
+        productViewModel.loadProductData();
+
+        Mockito.verify(productsLiveDataObserver).onChanged(expectedProducts);
+    }
+
+    @Test
+    public void givenOrderServiceWithOneProduct_whenRemoveData_thenVerifyProductsLiveDataChanged() {
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(new Product(12, 3, "Superproduct", "Storage 5", 1));
+
+        Mockito.when(orderServiceMock.getProducts()).thenReturn(expectedProducts);
+
+        productViewModel.loadProductData();
+        productViewModel.removeProduct(1);
+
+        Mockito.verify(productsLiveDataObserver, Mockito.times(2)).onChanged(expectedProducts);
+    }
+
+    @Test
+    public void givenOrderServiceWithOneProduct_whenUpdateData_thenVerifyUserLiveDataChanged() {
+        List<Product> expectedProducts = new ArrayList<>();
+        expectedProducts.add(new Product(2, 4, "TestProduct", "aisle 9", 1));
+        Mockito.when(orderServiceMock.getProducts()).thenReturn(expectedProducts);
+
+        productViewModel.loadProductData();
+        productViewModel.updateProduct(1,"Pillow", 2, "aisle 9", 4);
+        Mockito.verify(productsLiveDataObserver, Mockito.times(2)).onChanged(expectedProducts);
+    }
+
+    @Test
+    public void givenOrderServiceWithNoProduct_whenCreateData_thenVerifyUserLiveDataChanged() {
+        List<Product> expectedProducts = Collections.emptyList();
+
+        Mockito.when(orderServiceMock.getProducts()).thenReturn(expectedProducts);
+
+        productViewModel.loadProductData();
+        productViewModel.createProduct("TestProduct", 4, "aisle 9", 2);
+
+        Mockito.verify(productsLiveDataObserver, Mockito.times(2)).onChanged(expectedProducts);
     }
 }
